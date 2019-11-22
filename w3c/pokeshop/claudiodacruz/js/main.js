a3af422929f759929a93629f77afe4cf19b8fc5f
claudiodacruz <claudio.cruz@academico.ifpb.edu.br>
2019-11-22 16:33:43 -0300

    incio pokemon

--- w3c/pokeshop/claudiodacruz/js/main.js
+++ w3c/pokeshop/claudiodacruz/js/main.js
@@ -1,7 +1,11 @@
 const listaPokemon = document.querySelector('#pokemons')
 
-for (pokemon in pokemons){
-
-    result = `${pokemon['img']} ${pokemon['name']} ${pokemon['price']}`;
+for (pokemon of pokemons){
+    result = `<div class="pokemon-view">
+    <img class="pokemon-view-img" src="${pokemon.img}" alt="Bulbasaur">
+    <span class="pokemon-view-name">${pokemon.name}</span>
+    <span class="pokemon-view-price">R$ ${pokemon.price}</span>
+    <button class="pokemon-view-shop">COMPRAR</button>
+  </div>`;
     listaPokemon.insertAdjacentHTML('beforeend', result);
 }
